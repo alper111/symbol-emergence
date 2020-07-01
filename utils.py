cf288209e267490e28ad3fa81df077d61b61c496
Alper Ahmetoğlu <ahmetoglu.alper@gmail.com>
2020-07-01 15:38:34 +0300

    add state construction method

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 """Utility functions for RL training."""
 import torch
+import numpy as np
 
 
 def discount(rewards, gamma):
@@ -25,3 +26,36 @@ def discount(rewards, gamma):
         discounted_rewards.insert(0, R)
     discounted_rewards = torch.tensor(discounted_rewards, device=rewards.device)
     return discounted_rewards
+
+
+def construct_state(world, robot, device="cpu"):
+    """
+    Construct state as a tensor given world and robot.
+
+    State is the concatenation of:
+        - XY coordinates of the goal (2 dim)
+        - XY coordinates of the tip position (2 dim)
+        - Pose information of other objects (7 * n_obj dim)
+        - Joint angles (7 dim)
+
+    Parameters
+    ----------
+    world : env.Environment object
+        Environment instance.
+    robot : torobo_wrapper.Torobo object
+        Torobo instance.
+    device : string or torch.device
+        Device of state tensor.
+
+    Returns
+    -------
+    state : torch.tensor
+        State tensor.
+    """
+    tip_x = np.array(robot.get_tip_pos()[:2])
+    joint_angles = robot.get_joint_angles()
+    object_x = world.get_state().reshape(-1, 7)
+    object_x[:, :2] = object_x[:, :2] - tip_x
+    x = np.concatenate([object_x[0, :2], tip_x, joint_angles, object_x[1:].reshape(-1)])
+    x = torch.tensor(x, dtype=torch.float, device=device)
+    return x
